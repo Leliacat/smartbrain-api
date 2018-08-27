362d30fcb4b964854a2b868d832be130eb45db82
Leliacat <wattezlelia@yahoo.fr>
2018-08-27 15:27:41 +0200

    connecting to pg database with knex on /register /profile/:id and /image entry points

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const knex = require('knex');
 
 const app = express();
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'lelia',
+      password : '',
+      database : 'smart-brain'
+    }
+  });
 
 const database = {
     users: [
@@ -43,47 +53,44 @@ app.post('/signin',(req, res)=> {
     
 })
 
-
-
 app.post('/register',(req, res)=> {
     const { name, email } = req.body;
-    database.users.push({
-        id: '125',
-        name: name,
-        email: email,
-        entries: 0,
-        joined: new Date()
-    });
-    res.json(database.users[database.users.length-1]);
+   db('users')
+    .returning('*')
+    .insert({
+       email: email,
+       name: name,
+       joined : new Date()
+   })
+    .then(response =>{
+        res.json(user[0]);
+    })
+    .catch(err => res.status(400).json('unable to register'))
+    
 })
 
 app.get('/profile/:id',(req, res)=> {
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if(user.id === id) {
-            found = true;
-            return res.json(user)
-        } 
+    db.select('*').from('users').where({id})
+      .then(user => {
+        if (user.length){
+            res.json(user[0])
+        }else {
+            res.status(400).json ('not found')
+        }
     })
-    if (!found) {
-        res.status(400).json ('not found');
-    }
+      .catch(err => res.status(400).json ('error getting user'))
 })
 
 app.put('/image', (req, res)=> {
     const { id } = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if(user.id === id) {
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        } 
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0]);
     })
-    if (!found) {
-        res.status(400).json ('not found');
-    }
+    .catch(err => res.status(400).json('unable to get entrie'))
 })
 
 
